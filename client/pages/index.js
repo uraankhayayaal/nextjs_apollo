4892cc64f2c874cfca67c29724867c17afbbb09c
uraankhayayaal <uraankhayayaal@gmail.com>
2020-02-19 20:40:55 +0300

    fixes

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,26 +6,32 @@ import { useQuery } from '@apollo/react-hooks'
 import { useRouter } from 'next/router'
 
 const ViewerQuery = gql`
-  query ViewerQuery {
-    viewer {
-      id
-      email
+  query{
+    books{
+      title
+      author
     }
   }
 `
 
 const Index = () => {
   const router = useRouter()
-  const { data, loading } = useQuery(ViewerQuery)
+  const { data, loading } = useQuery(ViewerQuery, {
+    variables: { page: 0 }
+  })
 
-  //if (data && data.viewer) {
+  if (data) {
     return (
       <div>
         <p>Фнффд</p>
-        <p>Лфздшт</p>
+        {data.books.map((book, i) => {     
+           console.log("Entered");                 
+           // Return the element. Also pass key     
+           return (<div><b key={i} >{book.author}</b><p key={i} >{book.title}</p></div>) 
+        })}
       </div>
     )
-  //}
+  }
 
   return <p>Loading...</p>
 }
